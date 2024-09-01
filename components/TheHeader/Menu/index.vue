733cdb90f1a974d50db071f07ce6c353418fa9aa
Noga Vigdor <noga.vigdor@gmail.com>
2024-09-01 22:42:07 +0200

    added basic shield pages

---
+++ components/TheHeader/Menu/index.vue
@@ -0,0 +1,9 @@
+<template>
+    <div class="menu">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+        <router-link to="/contact">Contact</router-link>
+    </div>
+</template>
+<script setup lang="ts">
+</script>
