02a99b8f6a53b7c5a31648c82c0397462ce4a1b0
Noga Vigdor <noga.vigdor@gmail.com>
2024-09-05 00:25:49 +0200

    defining sections

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,33 @@
 <template>
     <div>
-        <h1>Index Page</h1>
+      <!-- Hero Section -->
+      <section id="hero">
+        <HeroSection />
+      </section>
+  
+      <!-- About Section -->
+      <section id="about" class="my-20">
+        <AboutSection />
+      </section>
+  
+      <!-- FAQ Section -->
+      <section id="faq" class="my-20">
+        <FAQSection />
+      </section>
+  
+      <!-- Signup Section -->
+      <section id="signup" class="my-20">
+        <SignupSection />
+      </section>
+  
+      <!-- Blog Reference -->
+      <section id="blog" class="my-20">
+        <BlogPreview />
+      </section>
     </div>
-</template>
-<script setup lang="ts">
-</script>
+  </template>
+  
+  <script setup lang="ts">
+  
+  </script>
+  
