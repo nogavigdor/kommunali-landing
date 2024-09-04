41ade7ca325c3ce3fcd47094dd92e1e07b0d5083
Noga Vigdor <noga.vigdor@gmail.com>
2024-09-05 00:24:56 +0200

    based structure of the landing page

--- app.vue
+++ app.vue
@@ -1,8 +1,32 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <TheHeaderMenu />
-    <NuxtPage />
-    
+    <!-- Hero Section -->
+    <section id="hero">
+      <HeroSection />
+    </section>
+
+    <!-- About Section -->
+    <section id="about" class="my-20">
+      <AboutSection />
+    </section>
+
+    <!-- FAQ Section -->
+    <section id="faq" class="my-20">
+      <FAQSection />
+    </section>
+
+    <!-- Signup Section -->
+    <section id="signup" class="my-20">
+      <SignupSection />
+    </section>
+
+    <!-- Blog Reference -->
+    <section id="blog" class="my-20">
+      <BlogPreview />
+    </section>
   </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
