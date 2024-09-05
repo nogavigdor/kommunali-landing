fe52220f9d9456bffaf7a916632c38b20b864ee7
Noga Vigdor <noga.vigdor@gmail.com>
2024-09-05 02:46:10 +0200

    adjusted the height of the carousel

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,9 +1,49 @@
 <template>
-    <div class="relative bg-gray-100 h-screen flex items-center justify-center">
-      <div class="text-center">
-        <h1 class="text-4xl font-bold">Explore Your Neighborhood's Hidden Gems</h1>
-        <p class="mt-4 text-xl">Find second-hand items and giveaways just steps away.</p>
-      </div>
-    </div>
+    <section class="relative w-full"> 
+      <UCarousel 
+        ref="carouselRef"
+        v-slot="{ item }"
+        :items="items"
+        :ui="{ item: 'basis-full' }"
+        class="h-[75vh] rounded-lg overflow-hidden relative"
+        indicators
+      >
+        <div class="w-full flex items-center justify-center">
+          <img :src="item" class="w-full h-auto object-contain" draggable="false"> 
+        </div>
+      </UCarousel>
+    </section>
   </template>
+  
+  
+  <script setup>
+
+const items = [
+'/_nuxt/assets/hero1.webp',
+ '/_nuxt/assets/hero2.webp',
+ '/_nuxt/assets/hero3.webp',
+ '/_nuxt/assets/hero4.webp',
+'/_nuxt/assets/hero5.webp',
+'/_nuxt/assets/hero6.webp',
+]
+
+const carouselRef = ref()
+
+onMounted(() => {
+  setInterval(() => {
+    if (!carouselRef.value) return
+
+    if (carouselRef.value.page === carouselRef.value.pages) {
+      return carouselRef.value.select(0)
+    }
+
+    carouselRef.value.next()
+  }, 3000)
+})
+  </script>
+  
+  <style scoped>
+
+
+  </style>
   
