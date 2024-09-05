49ce4a7078a07debd670dced3cfe5acbbf4b941f
Noga Vigdor <noga.vigdor@gmail.com>
2024-09-05 02:45:34 +0200

    used the NavBar

--- app.vue
+++ app.vue
@@ -1,32 +1,17 @@
 <template>
   <div>
-    <!-- Hero Section -->
-    <section id="hero">
-      <HeroSection />
-    </section>
 
-    <!-- About Section -->
-    <section id="about" class="my-20">
-      <AboutSection />
-    </section>
+    <NavBar />
 
-    <!-- FAQ Section -->
-    <section id="faq" class="my-20">
-      <FAQSection />
-    </section>
-
-    <!-- Signup Section -->
-    <section id="signup" class="my-20">
-      <SignupSection />
-    </section>
-
-    <!-- Blog Reference -->
-    <section id="blog" class="my-20">
-      <BlogPreview />
-    </section>
+    <NuxtPage />
+    
   </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 
 </script>
+
+<style scoped>
+/* Add any global styling here if needed */
+</style>
