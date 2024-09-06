45efa524aaacd7604ef54ffdae3038a3847be19a
Noga Vigdor <noga.vigdor@gmail.com>
2024-09-06 21:42:38 +0200

    removed post blog previews from the sections on the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,25 @@
 <template>
-    <div>
-      <!-- Hero Section -->
-      <section id="hero">
-        <HeroSection />
-      </section>
-  
-      <!-- About Section -->
-      <section id="about" class="my-20">
-        <AboutSection />
-      </section>
-  
-      <!-- FAQ Section -->
-      <section id="faq" class="my-20">
-        <FaqSection />
-      </section>
-  
-      <!-- Signup Section -->
-      <section id="signup" class="my-20">
-        <SignupSection />
-      </section>
-  
-      <!-- Blog Reference -->
-      <section id="blog" class="my-20">
-        <BlogPreview />
-      </section>
-    </div>
-  </template>
-  
-  <script setup lang="ts">
-  
-  </script>
-  
+  <div>
+    <!-- Hero Section -->
+    <section id="hero">
+      <HeroSection />
+    </section>
+
+    <!-- About Section -->
+    <section id="about" class="my-20">
+      <AboutSection />
+    </section>
+
+    <!-- FAQ Section -->
+    <section id="faq" class="my-20">
+      <FaqSection />
+    </section>
+
+    <!-- Signup Section -->
+    <section id="signup" class="my-20">
+      <SignupSection />
+    </section>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
