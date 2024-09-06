3f8d1d41e167e970457ce6c4437d7e76496e73a5
Noga Vigdor <noga.vigdor@gmail.com>
2024-09-06 21:38:29 +0200

    display a specific blog post

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -1,3 +1,32 @@
 <template>
-    a blog post
+  <NuxtLayout>
+    <article v-if="post && post.body">
+      <h1>{{ post.title }}</h1>
+      <p>{{ post.excerpt }}</p>
+      <nuxt-content :document="post" />
+    </article>
+    <div v-else>
+      <p>No post found.</p>
+    </div>
+  </NuxtLayout>
 </template>
+
+<script setup>
+import { parseJsonText } from "typescript";
+
+// Set the page layout to 'blog'
+definePageMeta({
+  layout: "blog",
+});
+
+const route = useRoute();
+const { data: post } = await useAsyncData(
+  `blog-post-${route.params.slug}`,
+  () =>
+    queryContent("blog")
+      .where({ _path: `/blog/${route.params.slug}` })
+      .findOne()
+);
+
+console.log("The post content is:", post); // Check what is being fetched
+</script>
