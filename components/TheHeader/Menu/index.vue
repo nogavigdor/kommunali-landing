5e8e455ab612d304c73aeb5d76e9b80cb6c34b53
Noga Vigdor <noga.vigdor@gmail.com>
2024-09-08 11:39:24 +0200

    deleted unused components

--- components/TheHeader/Menu/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <div class="menu">
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
-        <router-link to="/contact">Contact</router-link>
-    </div>
-</template>
-<script setup lang="ts">
-</script>
