4296015a61ed81ef55ce8913d7138ba70a3f10ec
Noga Vigdor <noga.vigdor@gmail.com>
2024-09-27 10:26:13 +0200

    added gogle tag manager funcionality to track user behavior

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,11 +7,15 @@ export default defineNuxtConfig({
     },
   },
 
-  modules: ["@nuxt/ui", "@nuxt/content", '@nuxt/image',],
+  modules: ["@nuxt/ui", "@nuxt/content", '@nuxt/image',   'nuxt-gtag', ],
   typescript: {
     typeCheck: true,
   },
 
   plugins: ["@/plugins/vue-scrollto.ts"],
+ 
+  gtag: {
+    id: 'GTM-P22RFRV6', // Replace with your actual Google Tag ID
+  },
   compatibilityDate: "2024-09-04",
 });
