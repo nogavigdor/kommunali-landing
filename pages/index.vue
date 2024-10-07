083f61436e0ee0d23a521406104d50d59dfdd306
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-07 08:01:34 +0200

    added a missing div

--- pages/index.vue
+++ pages/index.vue
@@ -1,28 +1,31 @@
-    <template>
-    <div>
-        <!-- Hero Section -->
-        <section id="hero">
-        <HeroSection />
-        </section>
+<template>
+  <div>
+    <!-- Hero Section -->
+    <section id="hero">
+      <HeroSection />
+    </section>
 
-        <!-- About Section -->
-        <section id="about" class="my-20">
-        <AboutSection />
-        </section>
+    <!-- About Section -->
+    <section id="about" class="my-20">
+      <AboutSection />
+    </section>
 
-        <!-- FAQ Section -->
-        <section id="faq" class="my-20">
-        <FaqSection />
-        </section>
+    <!-- FAQ Section -->
+    <section id="faq" class="my-20">
+      <FaqSection />
+    </section>
 
-        <!-- Signup Section -->
-        <section id="signup" class="my-20">
-        <SignupSection />
-        </section>
-    </div>
-    <button v-scroll-to="'#hero'" class="fixed bottom-4 right-4 p-4 bg-blue-500 text-white rounded-full">
-        Scroll to Top
-        </button>
-    </template>
+    <!-- Signup Section -->
+    <section id="signup" class="my-20">
+      <SignupSection />
+    </section>
+  </div>
+  <button
+    v-scroll-to="'#hero'"
+    class="fixed bottom-4 right-4 p-4 bg-blue-500 text-white rounded-full"
+  >
+    Scroll to Top
+  </button>
+</template>
 
-    <script setup lang="ts"></script>
+<script setup lang="ts"></script>
