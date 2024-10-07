024599115024dcc2b424b92ee69c5d29088547a1
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-07 12:02:10 +0200

    Add scroll to top button on homepage

--- pages/index.vue
+++ pages/index.vue
@@ -19,13 +19,14 @@
     <section id="signup" class="my-20">
       <SignupSection />
     </section>
+
+    <button
+      v-scroll-to="'#hero'"
+      class="fixed bottom-4 right-4 p-4 bg-blue-500 text-white rounded-full"
+    >
+      Scroll to Top
+    </button>
   </div>
-  <button
-    v-scroll-to="'#hero'"
-    class="fixed bottom-4 right-4 p-4 bg-blue-500 text-white rounded-full"
-  >
-    Scroll to Top
-  </button>
 </template>
 
 <script setup lang="ts"></script>
