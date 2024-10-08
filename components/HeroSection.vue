f4022f25808b22da9268637fb8330dbb0523ed4a
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-08 02:53:06 +0200

    removed minimum screen highet from the class

--- components/HeroSection.vue
+++ components/HeroSection.vue
@@ -1,49 +1,47 @@
-    <template>
-        <section class="min-h-screen relative w-full"> 
-        <UCarousel 
-            ref="carouselRef"
-            v-slot="{ item }"
-            :items="items"
-            :ui="{ item: 'basis-full' }"
-            class="h-[75vh] rounded-lg overflow-hidden relative"
-            indicators
-        >
-            <div class="w-full flex items-start justify-center">
-            <img :src="item" class="w-full h-auto object-contain" draggable="false"> 
-            </div>
-        </UCarousel>
-        </section>
-    </template>
-    
-    
-    <script setup>
-    import hero1 from '@/assets/hero1.webp'
-    import hero2 from '@/assets/hero2.webp'
-    import hero3 from '@/assets/hero3.webp'
-    import hero4 from '@/assets/hero4.webp'
-    import hero5 from '@/assets/hero5.webp'
-    import hero6 from '@/assets/hero6.webp'
-    
-const items = [hero1, hero2, hero3, hero4, hero5, hero6]
+<template>
+  <section class="relative w-full">
+    <UCarousel
+      ref="carouselRef"
+      v-slot="{ item }"
+      :items="items"
+      :ui="{ item: 'basis-full' }"
+      class="h-[75vh] rounded-lg overflow-hidden relative"
+      indicators
+    >
+      <div class="w-full flex items-start justify-center">
+        <img
+          :src="item"
+          class="w-full h-auto object-contain"
+          draggable="false"
+        />
+      </div>
+    </UCarousel>
+  </section>
+</template>
 
+<script setup>
+import hero1 from "@/assets/hero1.webp";
+import hero2 from "@/assets/hero2.webp";
+import hero3 from "@/assets/hero3.webp";
+import hero4 from "@/assets/hero4.webp";
+import hero5 from "@/assets/hero5.webp";
+import hero6 from "@/assets/hero6.webp";
 
-    const carouselRef = ref()
+const items = [hero1, hero2, hero3, hero4, hero5, hero6];
 
-    onMounted(() => {
-    setInterval(() => {
-        if (!carouselRef.value) return
+const carouselRef = ref();
 
-        if (carouselRef.value.page === carouselRef.value.pages) {
-        return carouselRef.value.select(0)
-        }
+onMounted(() => {
+  setInterval(() => {
+    if (!carouselRef.value) return;
 
-        carouselRef.value.next()
-    }, 3000)
-    })
-    </script>
-    
-    <style scoped>
+    if (carouselRef.value.page === carouselRef.value.pages) {
+      return carouselRef.value.select(0);
+    }
 
+    carouselRef.value.next();
+  }, 3000);
+});
+</script>
 
-    </style>
-    
+<style scoped></style>
