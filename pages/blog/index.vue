121dadbc8860ebe0200c91c2e5af04a6153a3b94
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-08 02:51:38 +0200

    added upper padding to section to the navigation bar will not cover the top part of the article's text

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -1,19 +1,21 @@
 <template>
-  <section class="blog-preview-section">
-    <h2 class="text-3xl font-bold text-gray-900 mb-6">Latest News</h2>
-    <div v-if="posts && posts.length" class="blog-list">
-      <article v-for="(post, index) in posts" :key="index" class="blog-item">
-        <h3 class="text-gray-700 font-semibold">{{ post.title }}</h3>
-        <p class="text-gray-600">{{ post.excerpt }}</p>
-        <NuxtLink :to="`${post._path}`" class="text-blue-500"
-          >Read More</NuxtLink
-        >
-      </article>
-    </div>
-    <div v-else>
-      <p>No posts found.</p>
-    </div>
-  </section>
+  <div class="pt-24">
+    <section class="blog-preview-section">
+      <h2 class="text-3xl font-bold text-gray-900 mb-6">Latest News</h2>
+      <div v-if="posts && posts.length" class="blog-list">
+        <article v-for="(post, index) in posts" :key="index" class="blog-item">
+          <h3 class="text-gray-700 font-semibold">{{ post.title }}</h3>
+          <p class="text-gray-600">{{ post.excerpt }}</p>
+          <NuxtLink :to="`${post._path}`" class="text-blue-500"
+            >Read More</NuxtLink
+          >
+        </article>
+      </div>
+      <div v-else>
+        <p>No posts found.</p>
+      </div>
+    </section>
+  </div>
 </template>
 
 <script setup>
