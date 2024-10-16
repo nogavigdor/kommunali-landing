620da781d2265792e70abbb22a500eb7648a2f16
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-16 05:46:38 +0200

    added  thumbnal to blog index page and re-styled the posts entroes

--- pages/blog/index.vue
+++ pages/blog/index.vue
@@ -4,11 +4,23 @@
       <h2 class="text-3xl font-bold text-gray-900 mb-6">Latest News</h2>
       <div v-if="posts && posts.length" class="blog-list">
         <article v-for="(post, index) in posts" :key="index" class="blog-item">
-          <h3 class="text-gray-700 font-semibold">{{ post.title }}</h3>
-          <p class="text-gray-600">{{ post.excerpt }}</p>
-          <NuxtLink :to="`${post._path}`" class="text-blue-500"
-            >Read More</NuxtLink
-          >
+          <img
+            :src="`/images/blog-images/${post.thumbnail}`"
+            alt="Post Thumbnail"
+            class="thumbnail-image w-48 h-auto object-cover rounded-lg shadow-md"
+            v-if="post.thumbnail"
+          />
+          <div class="content">
+            <h3 class="text-xl text-gray-700 font-semibold">
+              {{ post.title }}
+            </h3>
+            <p class="text-gray-600">{{ post.excerpt }}</p>
+            <NuxtLink
+              :to="`${post._path}`"
+              class="text-blue-500 hover:text-blue-700 transition-colors"
+              >Read More</NuxtLink
+            >
+          </div>
         </article>
       </div>
       <div v-else>
@@ -20,7 +32,7 @@
 
 <script setup>
 const { data: posts } = await useAsyncData("blog-posts", () =>
-  queryContent("blog").only(["title", "excerpt", "_path"]).find()
+  queryContent("blog").only(["title", "excerpt", "_path", "thumbnail"]).find()
 );
 </script>
 
@@ -38,5 +50,21 @@ const { data: posts } = await useAsyncData("blog-posts", () =>
   padding: 1.5rem;
   border-radius: 8px;
   box-shadow: 0 2px 10px rgba(0, 0, 0, 0.05);
+  display: flex;
+  align-items: flex-start;
+  gap: 1.5rem;
+}
+.thumbnail-image {
+  flex-shrink: 0;
+  width: 12rem;
+  height: auto;
+  object-fit: cover;
+  border-radius: 0.5rem;
+}
+
+.content {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 }
 </style>
