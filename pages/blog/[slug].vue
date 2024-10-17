cb3021f76c2ce499e338b8706fe102715662d824
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-17 13:36:59 +0200

    Updated blog post page styling and added breadcrumb navigation

--- pages/blog/[slug].vue
+++ pages/blog/[slug].vue
@@ -3,12 +3,22 @@
     <!-- Post container with gradient background -->
     <!-- Full-screen gradient background with padding -->
     <div
-      class="flex justify-center items-center min-h-screen bg-gradient-to-r from-purple-400 via-pink-500 to-red-500 py-16 px-4"
+      class="flex justify-center items-center min-h-screen bg-gradient-to-r from-primary to-secondary py-16 px-4"
     >
       <!-- Post content card -->
       <div
         class="max-w-3xl w-full bg-white shadow-2xl rounded-lg overflow-hidden"
       >
+       <!-- Breadcrumb -->
+       <div class="p-4 bg-gray-100">
+          <nav class="text-sm">
+            <NuxtLink to="/blog" class="text-darkLink underline hover:hoverDarkLink font-bold ">
+              Blog
+            </NuxtLink>
+            <span class="mx-2 text-gray-500">/</span>
+            <span class="text-gray-700">{{ post.title }}</span>
+          </nav>
+        </div>
         <!-- Post Header with Image (Optional) -->
         <div v-if="`/images/blog-images/${post.thumbnail}`" class="relative">
           <img
@@ -19,7 +29,7 @@
           <div
             class="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center"
           >
-            <h1 class="text-4xl font-extrabold text-white">{{ post.title }}</h1>
+            <h1 class="text-shadow ml-4 mr-4 text-4xl font-extrabold text-white">{{ post.title }}</h1>
           </div>
         </div>
         <div v-else class="p-8">
@@ -28,16 +38,16 @@
           </h1>
         </div>
         <!-- Post Content -->
-        <div class="p-8">
+        <div class="p-8 md:p-2">
           <!-- Post Metadata -->
           <div
             v-if="post.date || post.author"
             class="flex items-center justify-between mb-6"
           >
-            <p v-if="post.date" class="text-sm text-gray-500">
+            <p v-if="post.date" class="text-sm text-gray-800 font-bold">
               Published on {{ post.date }}
             </p>
-            <p v-if="post.author" class="text-sm text-gray-500">
+            <p v-if="post.author" class="text-sm text-gray-800 font-bold">
               Author: {{ post.author }}
             </p>
           </div>
@@ -67,3 +77,9 @@ const { data: post } = await useAsyncData(
 
 console.log("The post content is:", post.value); // Check what is being fetched
 </script>
+<style>
+.text-shadow {
+  text-shadow: 6px 6px 6px rgba(232, 255, 118, 0.75);
+ 
+}
+</style>
