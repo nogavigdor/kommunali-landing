a47a47f052f1d7ed8de7976b305bf1d28f3c19b7
Noga Vigdor <noga.vigdor@gmail.com>
2024-10-17 13:32:38 +0200

    updated color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,13 @@ export default {
         primary: '#FF5722', // Vibrant Orange for energy, action, and enthusiasm
         secondary: '#3F51B5', // Bold Indigo for creativity and trust
         accent: '#FFEB3B', // Bright Yellow for positivity and highlights
+        hoverAccent: '#ffef62', // Bright Yellow for highlights on hover
+        accentTwo: '#EE444A', // Vibrant Amber for secondary highlights
+        hoverAccentTwo: '#FFA000', // Vibrant Amber for secondary highlights on hover
+        darkLink: '#1A237E', // Dark Indigo for links 
+        hoverDarkLink: '#3949AB', // Dark Indigo for links on hover
+        brightLink: '#FF9800', // Bright Orange for links
+        hoverBrightLink: '#FFB74D', // Bright Orange for links on hover
         error: '#E53935', // Bold Red for error messages with urgency
         warning: '#FB8C00', // Vibrant Orange for warnings
         success: '#00E676', // Bright Green for successful actions
